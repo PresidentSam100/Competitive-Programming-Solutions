f168e4dc12ade3bf316e8a6269d17e394477dcf9
PresidentSam100 <56271444+PresidentSam100@users.noreply.github.com>
2022-04-03 09:14:14 -0500

    Add files via upload

---
+++ CSES/IntroductoryProblems/WeirdAlgorithms.cpp
@@ -0,0 +1,22 @@
+// Weird Algorithms
+// Sam Lu
+
+#include <iostream>
+using namespace std;
+
+long long int n;
+
+int main() {
+	cin >> n;
+	cout << n << " ";
+	while (n != 1) {
+		// Odd number
+		if (n & 1)
+			n = (3 * n) + 1;
+		// Even number
+		else
+			n >>= 1;
+		cout << n << " ";
+	}
+	return 0;
+}
---
+++ CSES/Sorting and Searching/DistinctNumbers.cpp
@@ -0,0 +1,20 @@
+// Distinct Numbers
+// Sam Lu
+
+#include <iostream>
+#include <set>
+using namespace std;
+
+long long int n, a;
+set<int> nums;
+
+int main() {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+	    cin >> a;
+		// Set will not insert duplicate values
+		nums.insert(a);
+	}
+	cout << nums.size();
+	return 0;
+}
