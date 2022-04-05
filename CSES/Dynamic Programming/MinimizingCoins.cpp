3ecd8503247266cc8b91a9c2febf2b5971c5d8fe
PresidentSam100 <56271444+PresidentSam100@users.noreply.github.com>
2022-04-05 14:31:35 -0500

    Add files via upload

--- CSES/Dynamic Programming/MinimizingCoins.cpp
+++ CSES/Dynamic Programming/MinimizingCoins.cpp
@@ -1,21 +1,42 @@
-// https://cses.fi/problemset/task/1633
-// Dice Combinations
+// https://cses.fi/problemset/task/1634
+// Minimizing Coins
+// Sam Lu
 
 #include <iostream>
+#include <climits>
+#include <array>
 #define lli long long int
 using namespace std;
 
-lli n;
-lli const MOD = 1000000007;
-lli dp[1000011];
+lli n, x, c;
+lli dp[1000001], coin[100];
 
 int main() {
-	cin >> n;
-	dp[10] = 1;
-	for (lli i = 11; i < 11 + n; i++) {
-		dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3] + dp[i - 4] + dp[i - 5] + dp[i - 6];
-		dp[i] %= MOD;
+	// Input
+	cin >> n >> x;
+	for (lli i = 0; i < n; i++) {
+		cin >> c;
+		coin[i] = c;
 	}
-	cout << dp[n + 10];
+	// Fill array with max value and set index 0 to value of 0
+	fill(dp, dp + x + 1, LLONG_MAX);
+	dp[0] = 0;
+	// Use dynamic programming to find all possible combinations
+	for (lli i = 1; i <= x; i++) {
+		for (int j = 0; j < n; j++) {
+			if (coin[j] <= i) {
+				lli temp = dp[i - coin[j]];
+				if (temp != LLONG_MAX && dp[i] - 1 > temp) {
+					dp[i] = temp + 1;
+				}
+			}
+		}
+	}
+	// Possible only when value is not LLONG_MAX
+	if (dp[x] == LLONG_MAX) {
+		cout << -1;
+	} else {
+		cout << dp[x];
+	}
 	return 0;
 }
